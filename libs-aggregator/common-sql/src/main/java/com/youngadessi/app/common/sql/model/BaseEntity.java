9d52f8f29244c71e3072e28e1c26c6345f65562f
berkgungor4 <berkgngr@hotmail.com>
2022-06-19 11:19:43 +0300

    Initial commit

---
+++ libs-aggregator/common-sql/src/main/java/com/youngadessi/app/common/sql/model/BaseEntity.java
@@ -0,0 +1,4 @@
+package com.youngadessi.app.common.sql.model;
+
+public abstract class BaseEntity {
+}
--- services-aggregator/user-service/src/main/java/com/youngadessi/app/user/service/entity/User.java
+++ services-aggregator/user-service/src/main/java/com/youngadessi/app/user/service/entity/User.java
@@ -1,4 +1,13 @@
 package com.youngadessi.app.user.service.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_user")
 public class User {
+
+    @Id
+    private int id;
 }
